ebf85929e8865c84acb5ae92f91d69700a493642
hu.bo <hoap@qq.com>
2022-12-12 15:51:55 +0800

    update cloudflare token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv'
 import { ChatGPTAPI } from 'chatgpt'
 import TelegramBot from 'node-telegram-bot-api'
 dotenv.config()
-const { token, sessionToken } = process.env
+const { token, sessionToken, clearanceToken, userAgent } = process.env
 
 const bot = new TelegramBot(token, { polling: true });
 console.log(new Date().toLocaleString(), '--Bot has been started...');
@@ -17,21 +17,25 @@ function msgHandler(msg) {
     case msg.text.indexOf('/start') === 0:
       bot.sendMessage(msg.chat.id, '👋你好！很高兴能与您交谈。有什么我可以帮您的吗？');
       break;
-    default:
+    case msg.text.length >= 2:
       chatGpt(msg, bot);
+      break;
+    default:
+      bot.sendMessage(msg.chat.id, '😭我不太明白您的意思。');
       break;
   }
 }
+
 async function chatGpt(msg, bot) {
   try {
-    const api = new ChatGPTAPI({ sessionToken })
+    const api = new ChatGPTAPI({ sessionToken, clearanceToken, userAgent })
     await api.ensureAuth()
     const response = await api.sendMessage(msg.text)
     console.log(response)
     bot.sendMessage(msg.chat.id, response, { parse_mode: 'Markdown' });
   } catch (err) {
     console.log(err)
-    bot.sendMessage(msg.chat.id, '😭出错了，我需要休息一下。');
+    bot.sendMessage(msg.chat.id, '😭出错了，请稍后再试；如果您是管理员，请检查日志。');
     throw err
   }
 }
